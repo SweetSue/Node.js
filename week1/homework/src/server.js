c748845a6d97f932dcd20bbba3d486e76c4c3b47
Marie-Christine Desplat <kiki_online@live.se>
2019-05-02 16:03:09 +0200

    Assignment: implement endpoints
    Missing writeHead

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -10,11 +10,36 @@ function createServer(port) {
 
   const server = http.createServer((request, response) => {
     // TODO: Write your homework code here
+    let result = 10;
+    let reset = 10;
+    if (req.url === '/') {
+      res.write('Hello les gars');
+      res.end();
+    } else if (req.url === '/Node_Mosh/first-app') {
+      res.write(JSON.stringify([1, 2, 3]));
+      res.end();
+    } else if (req.url === '/state') {
+      res.write(JSON.stringify({ state: result }));
+      res.end();
+    } else if (req.url === '/add') {
+      res.write(JSON.stringify({ state: result++ }));
+      res.end();
+    } else if (req.url === '/subtract') {
+      res.write(JSON.stringify({ state: result-- }));
+      res.end();
+    } else if (req.url === '/reset') {
+      res.write(JSON.stringify({ state: reset }));
+      res.end();
+    } else {
+      res.write(JSON.stringify({ error: 'Not found anywhere' }));
+      res.end();
+    }
+    // end TODO
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
